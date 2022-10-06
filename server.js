3453449ff1f1b363f0fa5fee8653c9932a02faa0
BalaKrishna Mangala <balakrishnamangala04@gmail.com>
2022-10-06 17:22:30 +0530

    balakrishna First Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+
+// connection to mongodb using mongoose
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser :true
+}).then(() =>{
+    console.log("DB Connected successfully....")
+}).catch(err =>{
+    console.log("Could not Connect DB successfully....",err)
+    process.exit();
+});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json())
+
+const AdminRoute = require('./app/routes/AdminRoutes')
+app.use('/admin',AdminRoute)
+
+app.listen(7070,function(){
+    console.log("Server is listening to Port no 7070");
+});
+
+app.get('/',(req,res)=>{
+    res.send("message:Hello Multiplex Node Express");
+});
+
+app.post('/display',(req,res)=>{
+    console.log("in post method")
+    console.log(req.body)
+});
